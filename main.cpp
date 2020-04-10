62472e4b54321571c300375d4dda2af5d98c6f6e
tenosoft <jkotlorz@protonmail.com>
2020-04-10 20:24:21 +0200

    Simple button handling added

--- main.cpp
+++ main.cpp
@@ -4,14 +4,35 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
-//Screen dimension constants
+enum KeyPressSurfaces
+{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 SDL_Window* window = NULL;
-SDL_Surface* screenSurface = NULL;
-SDL_Surface* gSplashScreen = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface* gCurrentSurface = NULL;
+
+SDL_Surface* loadSurface(std::string path)
+{
+	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+	if (loadedSurface == NULL) {
+		printf("Unable to load image %s!\n", path);
+	}
+
+	return loadedSurface;
+}
 
 bool init()
 {
@@ -32,8 +53,8 @@ bool init()
 		}
 		else
 		{
-			screenSurface = SDL_GetWindowSurface( window );
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 76, 0, 153 ) );
+			gScreenSurface = SDL_GetWindowSurface( window );
+			SDL_FillRect( gScreenSurface, NULL, SDL_MapRGB( gScreenSurface->format, 76, 0, 153 ) );
 
 		}
 	}
@@ -43,10 +64,14 @@ bool init()
 
 bool close()
 {
-	SDL_FreeSurface(gSplashScreen);
-	gSplashScreen = NULL;
+	for (int i = 0; i < KEY_PRESS_SURFACE_TOTAL; i++)
+	{
+		SDL_FreeSurface(gKeyPressSurfaces[i]);
+		gKeyPressSurfaces[i] = NULL;
+	}
 
 	SDL_DestroyWindow( window );
+	window = NULL;
 	SDL_Quit();
 }
 
@@ -54,13 +79,76 @@ bool load_media()
 {
 	bool success = true;
 
-	gSplashScreen = SDL_LoadBMP("splashscreen.bmp");
-	if (gSplashScreen == NULL) {
-		printf("Unable to load image!");
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("splashscreen.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL) {
+		printf("Failed to load DEFAULT image!\n");
 		success = false;
 	}
 
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("down.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL) {
+		printf("Failed to load DOWN image!\n");
+		success = false;
+	}
+
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("left.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL) {
+		printf("Failed to load LEFT image!\n");
+		success = false;
+	}
+
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("right.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL) {
+		printf("Failed to load RIGHT image!\n");
+		success = false;
+	}
+
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("up.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL) {
+		printf("Failed to load UP image!\n");
+		success = false;
+	}
 	return success;
+}
+
+void main_loop()
+{
+	bool quitReq = false;
+	SDL_Event e;
+	gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
+	while(!quitReq)
+	{
+		while(SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quitReq = true;
+			}
+			else if (e.type == SDL_KEYDOWN) {
+				switch (e.key.keysym.sym) {
+					case SDLK_DOWN:
+						gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+						break;
+					case SDLK_LEFT:
+						gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+						break;
+					case SDLK_RIGHT:
+						gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+						break;
+					case SDLK_UP:
+						gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+						break;
+					default:
+						gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+						break;
+						
+				}
+			}
+			SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(window);
+		}
+	}
 }
 
 int main( int argc, char* args[] )
@@ -71,9 +159,7 @@ int main( int argc, char* args[] )
 		if (!load_media()) {
 			printf("failed to load media!");
 		} else {
-			SDL_BlitSurface(gSplashScreen, NULL, screenSurface, NULL);
-			SDL_UpdateWindowSurface( window );
-			SDL_Delay( 2000 );
+			main_loop();
 		}
 	}
 
